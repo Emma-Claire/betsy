9590b3b540a6c040eeb27aca3a3f7e81f60bea0c
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-19 14:15:51 -0700

    Merchant model validation and relationship tests

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -4,7 +4,8 @@ describe Merchant do
 
   describe "validations" do
     it "requires a username" do
-      Merchant.new.valid?.must_equal false
+      merchant = Merchant.new
+      merchant.valid?.must_equal false
       merchant.errors.messages.must_include :username
     end
 
@@ -15,7 +16,9 @@ describe Merchant do
     end
 
     it "requires a email" do #success case
-
+      merchant = Merchant.new
+      merchant.valid?.must_equal false
+      merchant.errors.messages.must_include :email
     end
 
     it "requires a unique email" do #failure case
@@ -27,6 +30,11 @@ describe Merchant do
 
   describe "relations"do
     it "has a list of products" do
+      sally = merchants(:sally)
+      sally.must_respond_to :products
+      sally.products.each do |product|
+        product.must_be_kind_of Product
+      end
     end
   end
 end
