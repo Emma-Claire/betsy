286cae2ef92307d8d6d96363812a9c4fc9c69637
Cara Comfort <cara.comfort10@gmail.com>
2017-04-19 10:50:56 -0700

    Write relations and validations for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  has_many :reviews
+  has_many :orderedproducts
+  belongs_to :merchant
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than: 0 } #two decimal
+  # inventory than or equal to zero, int, presence true
+  validates :inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :category, presence: true
+  # photo_url -> presence or format?
 end
+#plants, planters, books, gardening, 
