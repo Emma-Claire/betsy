75489f5a63a8402303f8658ab64cabf3c4b49898
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-19 13:43:49 -0700

    initial tests for model test, deleted bad data from yml

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
-class MerchantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Merchant do
+
+  describe "validations" do
+    it "requires a username" do
+      Merchant.new.valid?.must_equal false
+      merchant.errors.messages.must_include :username
+    end
+
+    it "requires a unique username" do
+      merchants(:alice)
+      merchant_bad = Merchant.new(username: "alice")
+      merchant_bad.valid?.must_equal false
+    end
+
+    it "requires a email" do #success case
+
+    end
+
+    it "requires a unique email" do #failure case
+      merchants(:alice)
+      merchant_bad = Merchant.new(email: "alice@domainname.org")
+      merchant_bad.valid?.must_equal false
+    end
+  end
+
+  describe "relations"do
+    it "has a list of products" do
+    end
+  end
 end
