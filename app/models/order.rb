3286ae84e50a28b6077810655b213ae541341148
laura <lmelgarejos@unal.edu.co>
2017-04-19 13:51:11 -0700

    delete bad data from yml

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   has_many :orderedproducts
 
+  #inclusion forces presence to be true!!!!!
   validates :status, inclusion: {
     in: [ "pending", "paid", "shipped", "cancelled" ]
   }
