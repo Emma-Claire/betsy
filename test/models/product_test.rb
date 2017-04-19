35cf5b08fe73a0d2beec7cc0c93b88e234362a2c
Cara Comfort <cara.comfort10@gmail.com>
2017-04-19 13:39:33 -0700

    Add minitest to rails

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,16 +1,5 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  describe "relations" do
-    it "has a list of reviews" do
-      aloe = products(:aloe)
-      aloe.must_respond_to :reviews
-      aloe.reviews.
 
-    end
-
-    it "has a list of orderedproducts" do
-
-    end
-  end
 end
