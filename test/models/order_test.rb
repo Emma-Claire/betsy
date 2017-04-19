991b0a5ca69e1e50a128f2a31c170e5ef7281549
laura <lmelgarejos@unal.edu.co>
2017-04-19 15:45:46 -0700

    All tests passing

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,18 +1,29 @@
 require 'test_helper'
 
 describe Order do
+  let(:bad_order) { Order.create(status: "Lau") }
+  let(:blank_status_order) { Order.create(status: " ") }
+
+  describe "relationship with orderedproducts" do
+    it "one order has to have one or more orderedproducts" do
+      orders(:pending).must_respond_to :orderedproducts
+    end
+  end
+
   describe "Validations" do
-    it "must be valid" do
+    it "4 posible status must be valid" do
       orders(:pending).must_be :valid?
       orders(:paid).must_be :valid?
       orders(:shipped).must_be :valid?
       orders(:cancelled).must_be :valid?
-      orders(:fake).valid?.must_equal false
-      orders(:no_status).valid?.must_equal false
     end
 
-    it "Do not let create a new order without a status" do
-      Order.new.valid?.must_equal false
+    it "Do not let create a new order with a blank status" do
+      blank_status_order.valid?.must_equal false
+    end
+
+    it "Do not let create a new order without a right status" do
+      bad_order.valid?.must_equal false
     end
   end
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
-class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Review do
+  let(:bad_review) { Review.create }
+  let(:float_rating) { Review.create(rating: 0.1) }
+  let(:negative_review) { Review.create(rating: -0.1) }
+  let(:larger_review) { Review.create(rating: 6) }
+
+  describe "relationship with products" do
+    it "One review belongs to a product" do
+      reviews(:rating1).must_respond_to :product
+      reviews(:rating1).product.must_be_kind_of Product
+    end
+  end
+
+  describe "Validations" do
+    it "A review must have a rate in order to be created" do
+      bad_review.valid?.must_equal false
+    end
+
+    it "Rating must be an integer" do
+      float_rating.valid?.must_equal false
+    end
+
+    it "Rating must be greater_than 0" do
+      negative_review.valid?.must_equal false
+    end
+
+    it "Rating must be less than 6" do
+      larger_review.valid?.must_equal false
+    end    
+  end
 end
