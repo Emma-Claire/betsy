02c925412dc96f081de9dcc477adbfc86d437c49
Cara Comfort <cara.comfort10@gmail.com>
2017-04-20 16:24:11 -0700

    Write create route and method for orderedproducts

--- app/controllers/orderedproducts_controller.rb
+++ app/controllers/orderedproducts_controller.rb
@@ -1,2 +1,26 @@
 class OrderedproductsController < ApplicationController
+
+  def create
+    find_order
+    op = Orderedproduct.new(product_id: params[:product_id], order_id: @order.id, quantity: 1)
+    if op.save #may change this
+      flash[:success] = "Successfully added to cart"
+    else
+      flash[:failure] = "Unable to add item to cart"
+    end
+  end
+
+  private
+
+  def start_new_order
+    @order = Order.create(status: "pending")
+    session[:order_id] = @order.id
+  end
+
+  def find_order
+    @order = Order.find_by(id: session[:order_id])
+    if @order.nil?
+      start_new_order
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'products(/:category)', to: 'products#index', as: 'products', constraints: category_constraints
   get 'products/:id', to: 'products#show', as: 'product'
   get 'products/new', to: 'products#new', as: 'new_product'
-  
+
   post 'products', to: 'products#create'
 
   get 'products/:id/edit', to: 'products#edit', as: 'edit_product'
@@ -17,4 +17,10 @@ Rails.application.routes.draw do
   post 'products/:id/edit', to: 'products#edit'
   patch 'products/:id', to: 'products#update'
   resources :reviews, only: [:new, :create]
+
+
+  # orderedproduct routes
+  # get 'orderedproducts', to: 'orderedproducts#index', as: 'orderedproducts'
+
+  post 'orderedproducts/:product_id', to: 'orderedproducts#create', as: 'add_OP'
 end
