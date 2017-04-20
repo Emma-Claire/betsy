4165041d6bcae4914776b17ed4b01041c1385f88
laura <lmelgarejos@unal.edu.co>
2017-04-20 10:29:52 -0700

    wrote product's actions, routes and views for them

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,42 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to root_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+
+    if @product.nil?
+      head :not_found
+    end
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      head :not_found
+    else
+      @product.update_attributes(product_params)
+      if @product.save
+        redirect_to product_path(@product.id)
+      else
+        render :edit, status: :bad_request
+      end
+    end
+  end
+
+private
+  def product_params
+    return params.require(:product).permit(:name, :price, :category, :description, :inventory, :photo_url)
+  end
 end
