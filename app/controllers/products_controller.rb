b475205da69e001afd78c47df23d808f1f9fbc03
Cara Comfort <cara.comfort10@gmail.com>
2017-04-20 10:32:48 -0700

    Write product index and show route and controller methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+  def index
+    @category = params[:category]
+    @products = @category ? Product.where(category: @category) : Product.all
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
 end
