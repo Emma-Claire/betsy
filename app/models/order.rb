e9707bab1cfe3d63922b569a0d980ea7f7336d6b
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-20 16:27:22 -0700

    added start of validation for the payment info fields

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,14 @@ class Order < ApplicationRecord
   validates :status, inclusion: {
     in: [ "pending", "paid", "shipped", "cancelled" ]
   }
+
+# do this later
+  # validates :email,
+  # validates :mailing_address,
+  # validates :name_on_cc,
+  # validates :cc_num,
+  # validates :cc_exp,
+  # validates :cc_csv,
+  # validates :zip_code,
+
 end
