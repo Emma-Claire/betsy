62ff1c35fc894cbb672fc9b0e0976720055f69d7
Cara Comfort <cara.comfort10@gmail.com>
2017-04-22 20:42:39 -0700

    Add a link to leave a review from the product page

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,4 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6}
-  validates :comment, presence: true
 end
