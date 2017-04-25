b30d489bc2747d81a6abc50cf8a3c4e9a7d80831
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-04-24 17:11:21 -0700

    commented out check_inventory method/validation

--- app/models/orderedproduct.rb
+++ app/models/orderedproduct.rb
@@ -4,8 +4,9 @@ class Orderedproduct < ApplicationRecord
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
-  validates :quantity, numericality: { only_integer: true, greater_than: 0, less_than: :product.inventory }, on: :update
-  # 
+#need to wrote code to keep user from putting more items in cart than are in inventory
+  # validates :quantity, numericality: { only_integer: true, greater_than: 0, less_than: :product.inventory }, on: :update
+
   # def check_inventory
   #   product = Product.find_by(id: product_id)
   #   if quantity <= product.inventory
