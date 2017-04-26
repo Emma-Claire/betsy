872615540fe86ab7b53f419142fab7b9ca609a02
Emma Claire Philippides <acme9898@gmail.com>
2017-04-25 20:46:00 -0700

    tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,19 +1,19 @@
 require "simplecov"
 SimpleCov.start
-
 require "test_helper"
 
 describe ReviewsController do
   describe "new" do
     it "loads new_product_review_path" do
-      get new_product_review_path(products(:planters).id)
+      get new_product_review_path(products(:aloe).id)
       must_respond_with :success
     end
   end
 
   describe "create" do
-    it "adds a review to the database " do
-      start_count = Review.count
+    it "adds a product review to the database " do
+      product = Product.first
+      start_count = product.reviews.count
       review_params= {
         review: {
           rating: 3,
@@ -22,7 +22,7 @@ describe ReviewsController do
         }
       }
 
-      post product_product_id_reviews_path, params: review_params
+      post product_reviews_path, params: review_params
       must_redirect_to product_path(product_id)
 
       start_count.must_equal start_count + 1
@@ -31,30 +31,31 @@ describe ReviewsController do
     end
 
     it "responds with bad_request for bad data " do
-      start_count = Review.count
+      product = Product.first
+      start_count = product.reviews.count
       review_params= {
         review: {
           rating: "good"
         }
       }
-      post reviews_path, params: review_params
+      post product_reviews_path, params: review_params
       must_respond_with :bad_request
 
-      end_count = Review.count
+      end_count = Product.first.reviews.count
       end_count.must_equal start_count
     end
   end
 
   describe "show" do
-    it "succeeds for an existing reviewed product?)" do
-      review = Review.first
-      get :show, id: review, product_id: review.product_id
-      assert_response :success
+    it "succeeds for an existing reviewed product" do
+      review = Review.first.id
+      get product_review_path(review)
+      must_respond_with :success
     end
 
     it "renders 404 not found for a non-existing review" do
       bad_review_id = Review.last.id + 1
-      get product_review_path(bad_review_id)
+      get product_reviews_path(bad_review_id)
       must_respond_with :not_found
     end
   end
