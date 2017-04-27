56474f1ad5d0669dd097999a6e019235e0c8a6e2
Cara Comfort <cara.comfort10@gmail.com>
2017-04-26 18:59:58 -0700

    Implement method to modify product inventory and add method to orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,13 +7,13 @@ class Order < ApplicationRecord
     in: [ "pending", "paid", "shipped", "cancelled" ]
   }
 
-  validates :email, presence: true, on: :update
-  validates :mailing_address, presence: true, format: {with: /\A[a-zA-Z0-9]+\Z/}, on: :update
-  validates :name_on_cc, presence: true, format: {with: /\A[a-zA-Z]+\Z/}, on: :update
-  validates :cc_num, presence: true, numericality: { only_integer: true }, length: { is: 16 }, on: :update
-  validates :cc_exp, presence: true, numericality: { only_integer: true }, length: { is: 4 }, on: :update
-  validates :cc_csv, presence: true, numericality: { only_integer: true }, length: { minimum: 3, maximum: 4 }, on: :update
-  validates :zip_code, presence: true, numericality: { only_integer: true }, length: { is: 5 }, on: :update
+  # validates :email, presence: true, on: :update
+  # validates :mailing_address, presence: true, format: {with: /\A[a-zA-Z0-9]+\Z/}, on: :update
+  # validates :name_on_cc, presence: true, format: {with: /\A[a-zA-Z]+\Z/}, on: :update
+  # validates :cc_num, presence: true, numericality: { only_integer: true }, length: { is: 16 }, on: :update
+  # validates :cc_exp, presence: true, numericality: { only_integer: true }, length: { is: 4 }, on: :update
+  # validates :cc_csv, presence: true, numericality: { only_integer: true }, length: { minimum: 3, maximum: 4 }, on: :update
+  # validates :zip_code, presence: true, numericality: { only_integer: true }, length: { is: 5 }, on: :update
 
   def item_total
     orderedproducts.map { |op| op.quantity }.sum
@@ -28,6 +28,32 @@ class Order < ApplicationRecord
     return unavailable
   end
 
+  def modify_inventory(operator)
+    unless operator == "-" || operator == "+"
+      raise ArgumentError.new("Can only increase or decrease inventory")
+    end
+    # orderedproducts.each do |op|
+    #   product = Product.find_by(id: op.product_id)
+    #   if product.inventory >= op.quantity # double check that in stock
+    #     p.inventory -= op.quantity
+    #     p.save
+    #   else
+    #     return false
+    #   end
+    #   return true
+    # end
+    orderedproducts.each do |op|
+      product = Product.find_by(id: op.product_id)
+      # if operator == "-"
+      #   product.inventory -= op.quantity
+      # else
+      #   product.inventory += op.quantity
+      # end
+      product.inventory -= op.quantity if operator == "-"
+      product.inventory += op.quantity if operator == "+"
+      product.save
+    end
+  end
 
   def total
     t = 0
