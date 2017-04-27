afdad3040284ea601bd3106cb5462e77001f1b19
Cara Comfort <cara.comfort10@gmail.com>
2017-04-27 12:34:08 -0700

    Add shipped boolean to op model and set the default value to false

--- app/models/orderedproduct.rb
+++ app/models/orderedproduct.rb
@@ -6,7 +6,10 @@ class Orderedproduct < ApplicationRecord
 
 #need to wrote code to keep user from putting more items in cart than are in inventory
   # validates :quantity, numericality: { only_integer: true, greater_than: 0, less_than: (Product.find(:id product_id).inventory }, on: :update
+
   validate :cannot_put_more_items_in_cart_than_are_available_in_inventory, on: :update
+
+  after_initialize :set_defaults, unless: :persisted?
 
   def cannot_put_more_items_in_cart_than_are_available_in_inventory # ^_^
     product = Product.find_by(id: product_id)
@@ -15,15 +18,15 @@ class Orderedproduct < ApplicationRecord
     end
   end
 
-  def shipped
-    [true, false].sample
-    return false
-  end
-
   def subtotal
     '%.2f' % (product.price * quantity)
   end
 
   # assume only called on an array?
+
+  private
+  def set_defaults
+    self.shipped = false if self.shipped.nil?
+  end
 
 end
