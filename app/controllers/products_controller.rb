cc1961fcd05039c6c54ef17bf2f70a54da694e56
Cara Comfort <cara.comfort10@gmail.com>
2017-04-28 00:30:44 -0700

    DRYed out routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
+    # product.retired || product.inventory = 0
     @category = params[:category]
     @products = @category ? Product.where(category: @category) : Product.all
   end
