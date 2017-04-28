ce36b5b36c0e8e6b98c42a2b15909a8938a8fabc
laura <lmelgarejos@unal.edu.co>
2017-04-28 11:05:21 -0700

    logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # before_action :lookup_user
+  before_action :lookup_user
 
   private
   def lookup_user
