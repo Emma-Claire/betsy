f82a76483903da95c78b1b519fc7cecdad24abb8
laura <lmelgarejos@unal.edu.co>
2017-04-28 13:49:31 -0700

    added some tests

--- test/models/orderedproduct_test.rb
+++ test/models/orderedproduct_test.rb
@@ -1,6 +1,28 @@
 require 'test_helper'
 
 class OrderedproductTest < ActiveSupport::TestCase
+  describe "relations" do
+    let (:op) {Orderedproduct.new(quantity: 3)}
+    it "can set the order through 'order'" do
+      op.order = orders(:pending)
+      op.order_id.must_equal orders(:pending).id
+    end
+
+    it "can set the order through 'order_id'" do
+      op.order_id = orders(:pending).id
+      op.order.must_equal orders(:pending)
+    end
+
+    it "can set the product through 'product'" do
+      op.product = products(:century)
+      op.product_id.must_equal products(:century).id
+    end
+    it "can set the product through 'product_id'" do
+      op.product_id = products(:aloe).id
+      op.product.must_equal products(:aloe)
+    end
+  end
+
   describe "validations" do
     it "can be created with all attributes" do
       Orderedproduct.create!(
@@ -27,29 +49,6 @@ class OrderedproductTest < ActiveSupport::TestCase
       orderedproduct.valid?.must_equal false
       orderedproduct.errors.messages.must_include :quantity
     end
-
     # need test for negative numbers?
-  end
-
-  describe "relations" do
-    let (:op) {Orderedproduct.new(quantity: 3)}
-    it "can set the order through 'order'" do
-      op.order = orders(:pending)
-      op.order_id.must_equal orders(:pending).id
-    end
-
-    it "can set the order through 'order_id'" do
-      op.order_id = orders(:pending).id
-      op.order.must_equal orders(:pending)
-    end
-
-    it "can set the product through 'product'" do
-      op.product = products(:century)
-      op.product_id.must_equal products(:century).id
-    end
-    it "can set the product through 'product_id'" do
-      op.product_id = products(:aloe).id
-      op.product.must_equal products(:aloe)
-    end
   end
 end
